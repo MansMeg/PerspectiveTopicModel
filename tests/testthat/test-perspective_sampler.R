aa8d427eb7694fbf104d2a8db949fb767422be60
Måns Magnusson <mons.magnusson@gmail.com>
2017-01-14 10:10:51 +0100

    Add test with prior

--- tests/testthat/test-perspective_sampler.R
+++ tests/testthat/test-perspective_sampler.R
@@ -41,3 +41,31 @@ test_that("perspective_sampler", {
   params$verbose <- TRUE
   expect_output(res <- perspective_sampler(state_df, priors = priors, params))
 })
+
+
+test_that("perspective_sampler prior on Phi", {
+  set.seed(4711)
+  N <- 1000
+  D <- 17
+  V <- 31
+  K <- 10
+  P <- 3
+  state_df <- data.frame(doc = sample(1:D, size = N, replace = TRUE),
+                         type = factor(sample(1:V, size = N, replace = TRUE)),
+                         topic = sample(1:K, size = N, replace = TRUE),
+                         party = factor(sample(1:P, size = N, replace = TRUE)),
+                         perspective = sample(0:1, size = N, replace = TRUE))
+
+  priors <- priors(alpha = 0.1,
+                   betax0 = 0.01,
+                   betax1 = 0.01,
+                   alpha_pi = 0.1,
+                   beta_pi = 0.1,
+                   non_zero_type_topics = list("2" = c(1,2), "4" = 3))
+
+  params <- parameters(K = 5, gibbs_iter = 5L, save_state_every = 10, seed = 4711)
+
+  params$verbose <- FALSE
+  expect_silent(res <- perspective_sampler(state = state_df, priors = priors, params))
+
+})
