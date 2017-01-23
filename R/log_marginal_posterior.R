a38e68595381e1b02e52c4c08527662f8ef23c46
Måns Magnusson <mons.magnusson@gmail.com>
2017-01-23 16:38:34 +0100

    Added log marginal posterior

---
+++ R/log_marginal_posterior.R
@@ -0,0 +1,49 @@
+#' Log-Marginal posterior
+#'
+#' @description Compute the log marginal posterior for the perspective topic model.
+#'
+#' @param count_matrices a list of the following count matrices (with the same name): \code{n_dk}, \code{n_kvpx}, \code{n_kpx}, \code{n_pk}, \code{n_xk}
+#' @param priors a \code{prior} object.
+#'
+#' @keywords Internal
+log_marginal_posterior <- function(count_matrices, priors){
+  # Assertions
+  checkmate::assert_class(priors, "priors")
+  checkmate::assert_names(names(count_matrices), permutation.of = c("n_dk", "n_kvpx", "n_kpx", "n_pk", "n_xk" ))
+
+  # Create data structures
+  n_dk <- count_matrices$n_dk
+  n_kvpx <- count_matrices$n_kvpx
+  n_kpx <- count_matrices$n_kpx
+  n_pk <- count_matrices$n_pk
+  n_xk <- count_matrices$n_xk
+  n_d <- apply(n_dk, 1, sum)
+
+  V <- constants$V
+  K <- constants$K
+  P <- constants$P
+
+  betax0 <- priors$betax0
+  betax1 <- priors$betax1
+  alpha_pi <- priors$alpha_pi
+  beta_pi <- priors$beta_pi
+  alpha <- priors$alpha
+
+  # Pre-calculations
+  beta_x0_sum <- betax0 * V
+  beta_x1_sum <- betax1 * V
+
+  lgamma_beta_x0 <- lgamma(betax0)
+  lgamma_beta_x0_sum <- lgamma(beta_x0_sum)
+  lgamma_beta_x1 <- lgamma(betax1)
+  lgamma_beta_x1_sum <- lgamma(beta_x1_sum)
+
+  a <- K * lgamma_beta_x0_sum - K * V * lgamma_beta_x0 + sum(lgamma(n_kvpx[,,1] + betax0)) - sum(lgamma(n_xk[1,] + beta_x0_sum))
+  b <- P * K * lgamma_beta_x1_sum - P * K * V * lgamma_beta_x1 + sum(lgamma(n_kvpx[,,-1] + betax1)) - sum(lgamma(n_kpx[,,-1] + beta_x1_sum))
+  c <- P * K * lgamma(alpha_pi + beta_pi) - P * K * (lgamma(alpha_pi) + lgamma(beta_pi)) + sum(lgamma(n_kpx[,,1] + beta_pi)) + sum(lgamma(n_kpx[,,-1] + alpha_pi)) - sum(lgamma(n_pk + alpha_pi + beta_pi))
+  d <- D * lgamma(K * alpha) - D * K * lgamma(alpha) + sum(lgamma(n_dk + alpha)) - sum(lgamma(n_d + K * alpha))
+
+  a + b + c + d
+}
+
+
---
+++ tests/testthat/log_marginal_posterior.R
@@ -0,0 +1,54 @@
+context("log_marginal_posterior")
+
+
+test_that("log_marginal_posterior", {
+  set.seed(4711)
+  N <- 1000
+  D <- 17
+  V <- 31
+  K <- 10
+  P <- 3
+  state_df <- data.frame(doc = sample(1:D, size = N, replace = TRUE),
+                         type = factor(sample(1:V, size = N, replace = TRUE)),
+                         topic = sample(1:K, size = N, replace = TRUE),
+                         party = factor(sample(1:P, size = N, replace = TRUE)),
+                         perspective = sample(0:1, size = N, replace = TRUE))
+
+  constants <- list(D = length(unique(state_df$doc)),
+                    V = length(unique(state_df$type)),
+                    K = length(unique(state_df$topic)),
+                    P = length(unique(state_df$party)),
+                    N = nrow(state_df))
+
+  expect_silent(
+    stopifnot(constants$D == D,
+              constants$V == V,
+              constants$K == K,
+              constants$P == P,
+              constants$N == N)
+  )
+
+  priors <- priors(alpha = 0.1,
+                   betax0 = 0.05,
+                   betax1 = 0.01,
+                   alpha_pi = 0.15,
+                   beta_pi = 0.2)
+
+  params <- parameters(K = 10, gibbs_iter = 100L, save_state_every = 1000, seed = 4711, verbose = FALSE)
+
+  count_matrices <- PerspectiveTopicModel:::init_count2_cpp(state_df, constants)
+  count_matrices[["n_pk"]] <- t(apply(count_matrices$n_kpx, MARGIN=c(1, 2), sum))
+  count_matrices[["n_xk"]] <- t(apply(count_matrices$n_kpx, MARGIN=c(1, 3), sum))
+
+  expect_silent(
+    lmp0 <- log_marginal_posterior(count_matrices, priors)
+  )
+  expect_equal(lmp0, -8260.744, tolerance = 0.01)
+
+  # Run 100 iterations and check lmp < lmp0
+  expect_silent(res <- perspective_sampler(state_df, priors, params))
+  expect_silent(
+    lmp100 <- log_marginal_posterior(res$count_matrices, priors)
+  )
+  expect_equal(lmp100, -4687.773, tolerance = 0.01)
+})
