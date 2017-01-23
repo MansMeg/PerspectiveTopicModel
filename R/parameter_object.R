01e133e41c8c6b0d01d89837ed40ac18a5f098ef
Måns Magnusson <mons.magnusson@gmail.com>
2017-01-23 21:22:53 +0100

    Added log marginal posterior in parameter object

--- R/parameter_object.R
+++ R/parameter_object.R
@@ -4,7 +4,8 @@
 #' @param K The number of topics.
 #' @param start_iter Start iteration number.
 #' @param gibbs_iter The number of Gibbs iterations
-#' @param save_state_every Store the state file every X iteration.
+#' @param log_marginal_posterior_every Store the state file every X iteration.
+#' @param save_state_every Store the state file, priors, parameters and log marginal posterior every X iteration.
 #' @param state_path Where to store the state file.
 #' @param seed The seed to use for the random draws
 #' @param verbose Should the sampler be verbose?
@@ -14,15 +15,16 @@
 #' @export
 parameters <- function(x = NULL,
                        K = 10,
-                       start_iter = 2L,
+                       start_iter = 1L,
                        gibbs_iter = 10L,
+                       log_marginal_posterior_every = 1L,
                        save_state_every = NULL,
                        state_path = "perspective_",
                        seed = as.integer(Sys.time()),
                        verbose = TRUE){
   if(!is.null(x)) {
     checkmate::assert_class(x, "list")
-    checkmate::assert_subset(names(x), c("K", "start_iter", "gibbs_iter", "save_state_every", "state_path", "seed", "verbose"))
+    checkmate::assert_subset(names(x), c("K", "start_iter", "gibbs_iter", "save_state_every", "state_path", "seed", "verbose", "log_marginal_posterior_every"))
   }
 
   params_obj <- list(K = K,
@@ -42,6 +44,7 @@ parameters <- function(x = NULL,
   # Assertions
   checkmate::assert_integerish(params_obj$K, lower = 2L)
   checkmate::assert_integerish(params_obj$gibbs_iter, lower = params_obj$start_iter)
+  checkmate::assert_integerish(params_obj$log_marginal_posterior_every, lower = 1L)
   checkmate::assert_integerish(params_obj$start_iter, lower = 2L, upper = params_obj$gibbs_iter)
   checkmate::assert_integerish(params_obj$save_state_every, lower = 1L, params_obj$gibbs_iter, null.ok = TRUE)
   checkmate::assert_path_for_output(params_obj$state_path)
