2bef4f15a842154868c1a622a41fb6b7c3399a27
Måns Magnusson <mons.magnusson@gmail.com>
2017-07-04 09:44:03 +0200

    Updated prior object to handle simulated annealing

--- R/prior_object.R
+++ R/prior_object.R
@@ -9,6 +9,7 @@
 #' @param non_zero_type_topics List of priors on wordtypes. See details.
 #' @param non_zero_doc_topics List of priors on documents. See details.
 #' @param perspective_topics Topics that contain perspectives. If \code{NULL}, all topics have perspectives. If \code{0}, no topic has perspectives.
+#' @param annealing_iterations If simulated annealing is used. Indicate at which iteration the same position in hyper-parameter prior vector that should be used.
 #'
 #' @details
 #' The parameter \code{non_zero_type_topics} is a list named with types with prior settings. Each element is a type that contains a vector of non_zero probability topics.
@@ -17,10 +18,10 @@
 #' @return A \code{priors} object
 #'
 #' @export
-priors <- function(x = NULL, alpha = 0.1, betax0 = 0.01, betax1 = 0.01, alpha_pi = 0.1, beta_pi = 0.1, non_zero_type_topics = NULL, non_zero_doc_topics = NULL, perspective_topics = NULL){
+priors <- function(x = NULL, alpha = 0.1, betax0 = 0.01, betax1 = 0.01, alpha_pi = 0.1, beta_pi = 0.1, non_zero_type_topics = NULL, non_zero_doc_topics = NULL, perspective_topics = NULL, annealing_iterations = NULL){
   if(!is.null(x)) {
     checkmate::assert_class(x, "list")
-    checkmate::assert_subset(names(x), c("alpha", "betax0", "betax1", "alpha_pi", "beta_pi", "non_zero_type_topics", "non_zero_doc_topics", "perspective_topics"))
+    checkmate::assert_subset(names(x), c("alpha", "betax0", "betax1", "alpha_pi", "beta_pi", "non_zero_type_topics", "non_zero_doc_topics", "perspective_topics", "annealing_iterations"))
     if(!is.null(x$non_zero_type_topics)) {
       checkmate::assert_class(x$non_zero_type_topics, "list")
     }
@@ -33,7 +34,8 @@ priors <- function(x = NULL, alpha = 0.1, betax0 = 0.01, betax1 = 0.01, alpha_pi
                     beta_pi = beta_pi,
                     non_zero_type_topics = non_zero_type_topics,
                     non_zero_doc_topics = non_zero_doc_topics,
-                    perspective_topics = perspective_topics)
+                    perspective_topics = perspective_topics,
+                    annealing_iterations = annealing_iterations)
 
   if(!is.null(x)){
     for(name in names(x)){
@@ -42,11 +44,22 @@ priors <- function(x = NULL, alpha = 0.1, betax0 = 0.01, betax1 = 0.01, alpha_pi
   }
 
   # Check valid parameters
-  checkmate::assert_number(prior_obj$alpha, lower = 0.000000001)
-  checkmate::assert_number(prior_obj$betax0, lower = 0.000000001)
-  checkmate::assert_number(prior_obj$betax1, lower = 0.000000001)
-  checkmate::assert_number(prior_obj$alpha_pi, lower = 0.000000001)
-  checkmate::assert_number(prior_obj$beta_pi, lower = 0.000000001)
+  if(is.null(prior_obj$annealing_iterations)){
+    checkmate::assert_number(prior_obj$alpha, lower = 0.000000001)
+    checkmate::assert_number(prior_obj$betax0, lower = 0.000000001)
+    checkmate::assert_number(prior_obj$betax1, lower = 0.000000001)
+    checkmate::assert_number(prior_obj$alpha_pi, lower = 0.000000001)
+    checkmate::assert_number(prior_obj$beta_pi, lower = 0.000000001)
+  } else {
+    checkmate::assert_integerish(prior_obj$annealing_iterations, lower = 1)
+    checkmate::assert(prior_obj$annealing_iterations[1] == 1)
+    checkmate::assert_numeric(prior_obj$alpha, lower = 0.000000001, len = length(prior_obj$annealing_iterations))
+    checkmate::assert_numeric(prior_obj$betax0, lower = 0.000000001, len = length(prior_obj$annealing_iterations))
+    checkmate::assert_numeric(prior_obj$betax1, lower = 0.000000001, len = length(prior_obj$annealing_iterations))
+    checkmate::assert_numeric(prior_obj$alpha_pi, lower = 0.000000001, len = length(prior_obj$annealing_iterations))
+    checkmate::assert_numeric(prior_obj$beta_pi, lower = 0.000000001, len = length(prior_obj$annealing_iterations))
+  }
+
   if(!is.null(prior_obj$non_zero_type_topics)) {
     checkmate::assert_class(prior_obj$non_zero_type_topics, "list")
     checkmate::assert_character(names(prior_obj$non_zero_type_topics),
@@ -125,4 +138,22 @@ prepare_prior_for_sampling <- function(priors, constants, vocabulary, doc_ids){
   }
 
   priors
+}
+
+
+#' Get prior for iteration
+#'
+#' @details
+#' When running simulated annealing we have extract one specific prior per iteration.
+#'
+#' @param priors a \code{priors} object.
+#' @param iteration current iteration.
+#'
+#' @keywords Internal
+get_priors_for_iteration <- function(priors, iteration){
+  checkmate::assert_class(priors, "priors")
+  checkmate::assert_integerish(iteration, len = 1)
+
+
+  it_priors
 }
