465dac66899c52c0e264696e70076c22d7193f63
Måns Magnusson <mons.magnusson@gmail.com>
2020-09-03 21:39:16 +0200

    Init experiment 3

---
+++ experiments/experiment3.R
@@ -0,0 +1,49 @@
+
+# Experiment 3:
+# Will simulated annealing end up in a better mode?
+library(PerspectiveTopicModel)
+source("experiments/experiment_functions.R")
+
+
+## Run experiment 3
+
+txt <- readRDS(file = "experiments/data/three-men-all.rds")
+crp <- experiment3_corpus(txt, c("en" = 1200, "fr" = 360, "sv" = 420), 10)
+names(crp)[4] <- "type"
+crp$type <- as.factor(crp$type)
+
+tab <- table(res$lang)
+tab;sum(tab)
+
+N <- nrow(crp)
+
+# Create corpus versions
+crp1 <- crp
+crp1$chapter[crp1$chapter <= 10] <- 1
+crp1$chapter[crp1$chapter > 10] <- 2
+crp1$doc <- as.factor(paste0(crp1$lang, crp1$chapter))
+table(crp1$doc)
+
+crp2 <- crp
+crp2$chapter[crp2$chapter <= 10 & crp2$lang != "en"] <- 1
+crp2$chapter[crp2$chapter > 10 & crp2$lang != "en"] <- 2
+crp2$doc <- as.factor(paste0(crp2$lang, crp2$chapter))
+table(crp2$doc)
+
+crp3 <- crp
+crp3$doc <- as.factor(paste0(crp3$chapter))
+table(crp3$doc)
+
+
+experiment3 <- expand.grid(kappa = c(N, 2^(0:6)),
+                           C_kappa = 5,
+                           alpha = 1,
+                           init = "random",
+                           beta = 1,
+                           seed = 4711:5210,
+                           stringsAsFactors = FALSE)
+results1 <- run_experiment3(txt = crp1,
+                            experiment_jobs = experiment3,
+                            result_file_name = "results3_crp1.rda")
+
+
--- experiments/experiment_functions.R
+++ experiments/experiment_functions.R
@@ -27,10 +27,7 @@ generate_experiment1_corpus <- function(m, init = "word3", ndoc = NULL){
 #' The corpus is generated based on m, then SA is run for N * C_kappa itreations.
 #' Then the full Gibbs sampler (tau = 1) for
 run_experiment1 <- function(experiment_jobs, result_file_name){
-  checkmate::assert_data_frame(experiment_jobs)
-  must <- c("m", "kappa", "C_kappa", "init", "seed", "alpha", "beta")
-  can <- c("ndoc")
-  checkmate::assert_names(names(experiment_jobs), must.include = must, subset.of = c(must, can))
+  assert_experiment_jobs(experiment_jobs)
   checkmate::assert_path_for_output(result_file_name, overwrite = TRUE)
 
   if(!is.null(result_file_name) && file.exists(result_file_name)) {
@@ -120,28 +117,48 @@ assert_result_object <- function(x){
   checkmate::assert_true(x$sa$lmp[nrow(x$sa$lmp),]$log_post == x$final$lmp[1,]$log_post)
 }
 
-
-#install.packages("stopwords")
-#library(stopwords)
-#stopwords("fr", "nltk")
-# txt <- readRDS(file = "experiments/data/three-men-all.rds")
-experiment2_corpus <- function(txt, rare_word_limit = 10, lang_first_words = c("en" = 142, "fr" = 22, "sv" = 28), remove_stopwords = "nltk"){
+#' @param txt a tidy text corpus with columns [line], [chapter], [text] and [lang]
+#' @param no_words_per_chapter a named vector with language as name and the number of tokens to choose per chapter
+experiment3_corpus <- function(txt,
+                               no_words_per_chapter = c("en" = 700, "fr" = 210, "sv" = 300),
+                               rare_word_limit = 10,
+                               remove_stopwords = "nltk"){
   checkmate::assert_names(names(txt), must.include = c("line", "chapter", "text", "lang"))
+  checkmate::assert_names(names(no_words_per_chapter), subset.of = unique(txt$lang))
+  checkmate::assert_int(rare_word_limit, lower = 0)
+  checkmate::assert_integerish(no_words_per_chapter, lower = 0)
+  checkmate::assert_choice(remove_stopwords, null.ok = TRUE, stopwords::stopwords_getsources())
+  checkmate::assert_subset(names(no_words_per_chapter),  stopwords::stopwords_getlanguages(remove_stopwords))
+
   # Remove it and chapter titles
   txt <- txt[c(TRUE, !(txt$chapter[-length(txt$chapter)] != txt$chapter[-1])),]
   txt <- txt[txt$chapter >= 1,]
 
   txt <- tidytext::unnest_tokens(txt, word, text, token = "words", to_lower = TRUE)
 
-  # Remove stopwords here
-
+  # Add chapter row no
   txt <- dplyr::group_by(txt, lang, chapter)
   txt <- dplyr::mutate(txt, chapter_word_no = dplyr::row_number())
 
-  en <- remove_rare_words(get_first_n_words_from_chapter(txt, "en", 142), rare_word_limit)
-  fr <- remove_rare_words(get_first_n_words_from_chapter(txt, "fr", 22), rare_word_limit)
-  sv <- remove_rare_words(get_first_n_words_from_chapter(txt, "sv", 28), rare_word_limit)
+  res <- list()
+  for(i in seq_along(no_words_per_chapter)){
+    lang <- names(no_words_per_chapter)[i]
+    tmp <- txt[txt$lang == lang, ]
+    # Remove stopwords here
+    sw <- stopwords::stopwords(language = lang, source = remove_stopwords)
+    if(lang == "fr") sw <- c(sw, "qu’il", "a", "c’est", "d’un")
+    swt <- tibble(word = sw)
+
+    tmp <- dplyr::anti_join(tmp, swt, by = "word")
+
+    # Extract top words
+    tmp <- get_first_n_words_from_chapter(tmp, lang, no_words_per_chapter[lang])
+    res[[i]] <- remove_rare_words(tmp, rare_word_limit)
+  }
+  res <- do.call(rbind, res)
+  res
 }
+
 
 remove_rare_words <- function(txt, rare_word_limit){
   checkmate::assert_names(names(txt), must.include = c("line", "chapter", "word", "lang"))
@@ -162,4 +179,67 @@ get_first_n_words_from_chapter <- function(txt, lang, n){
   tmp <- txt[txt$lang == lang,]
   tmp <- tmp[tmp$chapter_word_no <= n,]
   tmp
+}
+
+
+#' Run experiment 1
+#'
+#' @description
+#' The corpus is generated based on m, then SA is run for N * C_kappa itreations.
+#' Then the full Gibbs sampler (tau = 1) for
+run_experiment3 <- function(txt, experiment_jobs, result_file_name){
+  assert_experiment_jobs(experiment_jobs)
+  checkmate::assert_path_for_output(result_file_name, overwrite = TRUE)
+  checkmate::assert_data_frame(txt)
+  checkmate::assert_names(names(txt), must.include = c("doc", "type", "lang"))
+
+  if(!is.null(result_file_name) && file.exists(result_file_name)) {
+    load(result_file_name, envir = parent.frame())
+    message("Results already exist. Return the current results.")
+    return(list(results = results, experiment_jobs = experiment_jobs))
+  }
+
+  # Setup experiment
+  crp <- txt
+  N <- nrow(crp)
+  experiment_jobs$tau <- experiment_jobs$kappa/N
+  experiment_jobs$sa_iterations <- N * experiment_jobs$C_kappa
+  experiment_jobs$iterations <- N
+
+  results <- list()
+  for(i in 1:nrow(experiment_jobs)){
+    print(paste(Sys.time(), ":", i))
+    checkmate::assert_choice(experiment_jobs$init[i], choices = c("random", "lang"))
+    if(experiment_jobs$init[i] == "random"){
+      crp$topic <- sample(1:3, N, TRUE)
+    }
+    if(experiment_jobs$init[i] == "lang"){
+      crp$topic <- as.integer(as.factor(crp$lang))
+    }
+
+    params <- list(K = 3,
+                   tau = rep(experiment_jobs$tau[i], experiment_jobs$sa_iterations[i]),
+                   save_state_every = NULL,
+                   log_marginal_posterior_every = 10,
+                   seed = experiment_jobs$seed[i],
+                   verbose = FALSE)
+    priors <- list(alpha = experiment_jobs$alpha[i], beta = experiment_jobs$beta[i])
+    # Run SA
+    res_sa <- collapsed_sampler_simulated_annealing(state = crp, priors = priors, params)
+    # Run final N iterations with tau = 1
+    params$tau <- rep(1, N)
+    res_final <- collapsed_sampler_simulated_annealing(state = res_sa$state, priors, params)
+    results[[i]] <- list(sa = res_sa, final = res_final)
+  }
+  save(results, experiment_jobs, file = result_file_name)
+
+  list(results = results, experiment_jobs = experiment_jobs)
+}
+
+
+assert_experiment_jobs <- function(experiment_jobs){
+  checkmate::assert_data_frame(experiment_jobs)
+  must <- c("kappa", "C_kappa", "seed", "alpha", "beta")
+  can <- c("m", "init", "ndoc")
+  checkmate::assert_names(names(experiment_jobs), must.include = must, subset.of = c(must, can))
 }
