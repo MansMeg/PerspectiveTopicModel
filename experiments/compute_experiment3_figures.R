8f8d39449859be46a280b1929aa99fc3471be5a9
Måns Magnusson <mons.magnusson@gmail.com>
2020-09-08 11:20:11 +0200

    Added experiment 3b figures

--- experiments/compute_experiment3_figures.R
+++ experiments/compute_experiment3_figures.R
@@ -80,5 +80,43 @@ save(plt, experiment_jobs, file = "kappa_experiments3_crp2_initlang.rda")
 
 
 
+rm(list = ls())
 
+source("experiments/experiment_functions.R")
+load("results3b_crp1.rda")
+
+experiment_jobs$max_lmp <- extract_max_log_marginal_posterior(results)
+experiment_jobs$mode <- "-20400"
+experiment_jobs$mode[experiment_jobs$max_lmp > -20200] <- "-20000"
+experiment_jobs$mode[experiment_jobs$max_lmp > -19600] <- "-19200"
+
+# which(experiment_jobs$mode == "-20000")
+# results[[10]]$final$count_matrices
+
+plt <- ggplot(data = experiment_jobs, aes(x = max_lmp)) + geom_histogram() + theme_bw() + xlab("Maximum Log Posterior Density") + ylab("Count") + scale_fill_grey()
+ggsave(filename = "kappa_experiments3b_crp1_histogram.png", plot = plt, width = 5, height = 3)
+save(plt, experiment_jobs, file = "kappa_experiments3b_crp1_histogram.rda")
+
+plt <- ggplot(data = experiment_jobs, aes(x = as.factor(kappa), fill = mode)) + geom_bar(position = position_dodge(preserve = 'single')) + theme_bw() + xlab(expression(kappa)) + ylab("Count") + scale_fill_grey()
+ggsave(filename = "kappa_experiments3b_crp1.png", plot = plt, width = 5, height = 3)
+save(plt, experiment_jobs, file = "kappa_experiments3b_crp1.rda")
+
+
+
+rm(list = ls())
+
+source("experiments/experiment_functions.R")
+load("results3b_crp2.rda")
+
+experiment_jobs$max_lmp <- extract_max_log_marginal_posterior(results)
+experiment_jobs$mode <- "-20400"
+experiment_jobs$mode[experiment_jobs$max_lmp > -19600] <- "-19200"
+
+plt <- ggplot(data = experiment_jobs, aes(x = max_lmp)) + geom_histogram() + theme_bw() + xlab("Maximum Log Posterior Density") + ylab("Count") + scale_fill_grey()
+ggsave(filename = "kappa_experiments3b_crp2_histogram.png", plot = plt, width = 5, height = 3)
+save(plt, experiment_jobs, file = "kappa_experiments3b_crp2_histogram.rda")
+
+plt <- ggplot(data = experiment_jobs, aes(x = as.factor(kappa), fill = mode)) + geom_bar(position = position_dodge(preserve = 'single')) + theme_bw() + xlab(expression(kappa)) + ylab("Count") + scale_fill_grey()
+ggsave(filename = "kappa_experiments3b_crp2.png", plot = plt, width = 5, height = 3)
+save(plt, experiment_jobs, file = "kappa_experiments3b_crp2.rda")
 
